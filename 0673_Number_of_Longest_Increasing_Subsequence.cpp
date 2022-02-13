ae79cdb8e4066318e8dd592bfd0ddf9c2229460f
ZhangRRz <85752688+ZhangRRz@users.noreply.github.com>
2022-02-13 19:12:05 +0800

    Dynamic Programming

---
+++ 0673_Number_of_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,62 @@
+/*
+source
+https://leetcode.com/problems/number-of-longest-increasing-subsequence/discuss/1341428/C%2B%2B-EASY-SIMPLE-SOLUTION-WITH-DETAILED-EXPLANATION
+
+
+*/
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        vector<pair<int,int>>dp; 
+        // dp[i]={a,b} represents that if we take ith element of nums as the last element 
+        // in a increasing sequence
+        // then a is the length of the biggest sequence
+        // and there can be b such sequence of length a and last element as nums[i]
+        dp.push_back({1,1});     //base case
+        for(int i=1;i<nums.size();i++)
+        {
+            int len=0;
+            int count=0;
+            for(int j=0;j<i;j++)
+            {
+                if(nums[j]<nums[i])  // to make sure that sequence is increasing
+                {                       
+                    // this is basically to find the maximum length subsequence
+                    // and find how many such sequences are possible
+                    
+                    if(len==dp[j].first)   //if max length we found till now is equal to current length
+                    {
+                        count+=dp[j].second;// we increment count( no. of combinations)
+                    }
+                    else if(dp[j].first>len) //if greater len is found
+                    {
+                        //then we set count to this seq count and len(maximum len) to its length 
+                        count=dp[j].second; 
+                        len=dp[j].first;     
+                    }
+                }
+            }
+            if(count==0)//  in case we dont find any smaller element before it 
+            {
+                dp.push_back({len+1,1});
+            }
+            else 
+                dp.push_back({len+1,count});
+        }
+        int len=0;
+        int count=0;
+        for(int i=0;i<dp.size();i++)// answer would be number of maximum length sequences    
+        {
+            if(len<dp[i].first)
+            {
+                len=dp[i].first;
+                count=dp[i].second;
+            }
+            else if(len==dp[i].first)
+            {
+                count+=dp[i].second;
+            }
+        }
+        return count;
+    }
+};

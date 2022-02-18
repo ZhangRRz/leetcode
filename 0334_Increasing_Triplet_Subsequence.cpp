7a58cd4528f5f82fb9896ec6e1ec015791e2ffca
ZhangRRz <85752688+ZhangRRz@users.noreply.github.com>
2022-02-18 15:08:16 +0800

    Array

---
+++ 0334_Increasing_Triplet_Subsequence.cpp
@@ -0,0 +1,26 @@
+
+/*
+ if the problem requires us to return the index, then this code would not work.
+*/
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        
+        int i = INT_MAX;
+        int j = INT_MAX;
+        
+        for(int k = 0;k < nums.size();k++){
+            
+            if(nums[k] <= i ){
+                i = nums[k];
+                
+            }else if(nums[k] <= j){
+                j = nums[k];
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+};

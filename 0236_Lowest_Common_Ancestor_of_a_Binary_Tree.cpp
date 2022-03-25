07d458c58276d5423523b6e1496234268531f22c
ZhangRRz <85752688+ZhangRRz@users.noreply.github.com>
2022-03-25 12:46:34 +0800

    Data Structure - Tree

---
+++ 0236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if( !root || root == p || root == q)
+            return root;
+        
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if(!left) // if left == nullptr
+            return right;
+        
+        else if(!right)  // if left == nullptr
+            return left;
+        
+        else  // left and right are not nullptr
+            return root;
+    }
+};

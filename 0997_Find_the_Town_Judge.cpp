b84b6791f862589d98c738e76048430ebd0da3fc
ZhangRRz <85752688+ZhangRRz@users.noreply.github.com>
2022-03-26 11:04:26 +0800

    Graph

--- 0997_Find_the_Town_Judge.cpp
+++ 0997_Find_the_Town_Judge.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        int numbers[n+1];
-        
-        for(int i = 1;i < n+1; i++){
-            numbers[i] = 0;
-        }
+        vector<int> numbers(n+1,0);
         for(vector<int> i: trust){
             numbers[i[0]] = -1;
             numbers[i[1]]++;

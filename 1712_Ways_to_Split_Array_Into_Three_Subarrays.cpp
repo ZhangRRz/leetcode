3d6f2cbe369b501bd2e2c5b8cd81128e7d4551ac
ZhangRRz <85752688+ZhangRRz@users.noreply.github.com>
2022-04-25 21:52:11 +0800

    tricky

---
+++ 1712_Ways_to_Split_Array_Into_Three_Subarrays.cpp
@@ -0,0 +1,22 @@
+/*
+https://leetcode.com/problems/ways-to-split-array-into-three-subarrays/discuss/999257/C%2B%2BJavaPython-O(n)-with-picture
+
+implementation is tricky, need more think
+*/
+class Solution {
+public:
+    int waysToSplit(vector<int>& nums) {
+        int res = 0, sz = nums.size();
+        partial_sum(begin(nums), end(nums), begin(nums));
+        for (int i = 0, j = 0, k = 0; i < sz - 2; ++i) {
+            j = max(i + 1, j);
+            while (j < sz - 1 && nums[j] < nums[i] * 2)
+                ++j;
+            k = max(j, k);
+            while (k < sz - 1 && nums[k] - nums[i] <= nums[sz - 1] - nums[k])
+                ++k;
+            res = (res + k - j) % 1000000007;
+        }
+        return res;
+    } 
+};

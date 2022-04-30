e5d17d627b054082ccdb46af8647c1bb0c178ae2
ZhangRRz <85752688+ZhangRRz@users.noreply.github.com>
2022-04-30 22:18:53 +0800

    Binary Search

---
+++ 0658_Find_K_Closest_Elements.cpp
@@ -0,0 +1,19 @@
+/*
+https://leetcode.com/problems/find-k-closest-elements/discuss/106426/JavaC%2B%2BPython-Binary-Search-O(log(N-K)-%2B-K)
+*/
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& A, int k, int x) {
+        
+        int left = 0, right = A.size() - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - A[mid] > A[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return vector<int>(A.begin() + left, A.begin() + left + k);
+    }
+};
